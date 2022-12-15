41562c6e51c9695878cd40b2b7318ddf5dec3160
M, Prabhu | RIEPL <prabhu.m@rakuten.com>
2022-12-15 14:50:51 +0530

    UI Change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:html' as html;
@@ -18,17 +20,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          appBarTheme: AppBarTheme(
+            color: Color.fromRGBO(140, 15, 13, 1),
+          ),
+          scaffoldBackgroundColor: Color.fromRGBO(140, 15, 13, 0.6)),
       home: const MyHomePage(title: 'Flutter PWA APP'),
     );
   }
@@ -36,15 +31,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -57,11 +43,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -76,65 +57,130 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Hello world from Flutter PWA ',
-                style: TextStyle(
-                    color: Colors.purple,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40)),
+            // RaisedButton(
+            //     child: Text('CMS Page',
+            //         style: TextStyle(
+            //             color: Colors.white,
+            //             fontWeight: FontWeight.bold,
+            //             fontSize: 40)),
+            //     color: Colors.blue,
+            //     onPressed: () async {
+            //       js.context
+            //           .callMethod('open', ['https://ticket.rakuten.co.jp/']);
+            //     }),
+            // RaisedButton(
+            //     child: Text('Performances',
+            //         style: TextStyle(
+            //             color: Colors.white,
+            //             fontWeight: FontWeight.bold,
+            //             fontSize: 40)),
+            //     color: Colors.blue,
+            //     onPressed: () async {
+            //       js.context
+            //           .callMethod('open', ['https://ticket.rakuten.co.jp/']);
+            //     })
 
-            // const Text(
-            //   'You have pushed the button this many times:',
-            // ),
-            // Text(
-            //   '$_counter',
-            //   style: Theme.of(context).textTheme.headline4,
-            // ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      onPressed: () async {
+                        js.context.callMethod(
+                            'open', ['https://ticket.rakuten.co.jp/']);
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      elevation: 1.5,
+                      color: Colors.white,
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Icon(
+                            Icons.developer_board,
+                            color: Color.fromRGBO(140, 15, 13, 0.9),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "CMS Website",
+                            style: TextStyle(
+                              color: Color.fromRGBO(140, 15, 13, 0.9),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  Expanded(
+                    child: RaisedButton(
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      elevation: 1.5,
+                      color: Colors.white,
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Icon(Icons.event,
+                              color: Color.fromRGBO(140, 15, 13, 0.9)),
+                          SizedBox(height: 10),
+                          Text(
+                            "Events",
+                            style: TextStyle(
+                              color: Color.fromRGBO(140, 15, 13, 0.9),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed:
-            // () async {
-            //   //final Uri _url = Uri.parse('https://ticket.rakuten.co.jp/');
-            //   final Uri _url = Uri.parse('https://ticket.rakuten.co.jp/');
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed:
+      //       // () async {
+      //       //   //final Uri _url = Uri.parse('https://ticket.rakuten.co.jp/');
+      //       //   final Uri _url = Uri.parse('https://ticket.rakuten.co.jp/');
 
-            //   if (!await launchUrl(
-            //     _url,
-            //     mode: LaunchMode.inAppWebView,
-            //   )) {
-            //     throw 'Could not launch $_url';
-            //   }
-            // },
-            () {
-          js.context.callMethod('open', ['https://ticket.rakuten.co.jp/']);
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      //       //   if (!await launchUrl(
+      //       //     _url,
+      //       //     mode: LaunchMode.inAppWebView,
+      //       //   )) {
+      //       //     throw 'Could not launch $_url';
+      //       //   }
+      //       // },
+      //       () {
+      //     js.context.callMethod('open', ['https://ticket.rakuten.co.jp/']);
+      //     html.window.addEventListener('load', listen, false);
+
+      //   },
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void listen(html.Event event) {
+    print("before timer");
+    Timer(Duration(seconds: 3), () {
+      print("after timer");
+      html.window.scrollTo(0, 1);
+    });
   }
 }
